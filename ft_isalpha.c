584bebd5d41f02f059710202aefaedba854cf459
Chanhee Lee <chanhlee@student.42seoul.kr>
2021-01-20 10:57:11 +0900

    modified norminette

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,13 +6,13 @@
 /*   By: chanhlee <chanhlee@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 18:58:24 by chanhlee          #+#    #+#             */
-/*   Updated: 2021/01/18 18:59:27 by chanhlee         ###   ########.fr       */
+/*   Updated: 2021/01/20 10:56:19 by chanhlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_isalpha(int c)
+int	ft_isalpha(int c)
 {
 	return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
 }
--- ft_isascii.c
+++ ft_isascii.c
@@ -6,13 +6,13 @@
 /*   By: chanhlee <chanhlee@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/19 16:59:21 by chanhlee          #+#    #+#             */
-/*   Updated: 2021/01/19 17:14:46 by chanhlee         ###   ########.fr       */
+/*   Updated: 2021/01/20 10:56:02 by chanhlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_isascii(int c)
+int	ft_isascii(int c)
 {
 	return (c >= 0 && c < 128);
 }
