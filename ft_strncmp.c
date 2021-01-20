1a5fe0852bc67ab5b1c37e7e634b7b42ddfb44a7
Chanhee Lee <chanhlee@student.42seoul.kr>
2021-01-20 12:02:14 +0900

    add strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,24 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: chanhlee <chanhlee@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/01/18 14:08:38 by chanhlee          #+#    #+#             */
-/*   Updated: 2021/01/20 11:49:37 by chanhlee         ###   ########.fr       */
+/*   Created: 2021/01/20 11:49:50 by chanhlee          #+#    #+#             */
+/*   Updated: 2021/01/20 12:01:36 by chanhlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *big, const char *little, size_t len)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t little_len;
-	int i;
+	size_t i;
 
-	if (little_len == 0)
-		return ((char *)big);
 	i = 0;
-	while (big[i] != '\0' && 
+	if (s1[i] != '\0' && s2[i] != '\0')
+	{
+		while (i < n && s1[i] == s2[i])
+			i++;
+	}
+	if (i == n)
+		return (0);
+	return (s1[i] - s2[i]);
 }
