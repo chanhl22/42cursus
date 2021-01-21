ab37d6b41c1bece3295e4bd218739a7080439e34
Chanhee Lee <chanhlee@student.42seoul.kr>
2021-01-21 17:26:07 +0900

    update libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: chanhlee <chanhlee@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 13:52:36 by chanhlee          #+#    #+#             */
-/*   Updated: 2021/01/20 12:28:59 by chanhlee         ###   ########.fr       */
+/*   Updated: 2021/01/21 17:25:51 by chanhlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,5 +37,7 @@ int		ft_isalnum(int c);
 int		ft_isprint(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+char	*ft_strdup(const char *s1);
+void	*ft_calloc(size_t count, size_t size);
 
 #endif
