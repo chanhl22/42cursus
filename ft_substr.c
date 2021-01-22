c1b8dd29aecd578840b3105e37fafaf2a4b93096
Chanhee Lee <chanhlee@student.42seoul.kr>
2021-01-22 21:29:34 +0900

    modified substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,24 +6,24 @@
 /*   By: chanhlee <chanhlee@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/22 20:40:36 by chanhlee          #+#    #+#             */
-/*   Updated: 2021/01/22 21:25:06 by chanhlee         ###   ########.fr       */
+/*   Updated: 2021/01/22 21:29:04 by chanhlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	size_t i;
-	char *prt;
-	
-	if(s == NULL)
+	size_t	i;
+	char	*prt;
+
+	if (s == NULL)
 		return (NULL);
-	prt = (char*)malloc(sizeof(*s)*(len + 1));
-	if(str == NULL)
+	prt = (char*)malloc(sizeof(*s) * (len + 1));
+	if (str == NULL)
 		return (NULL);
 	i = 0;
-	while(i < len)
+	while (i < len)
 	{
 		prt[i] = s[start + i];
 		i++;
