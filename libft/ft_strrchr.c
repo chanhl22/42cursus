60816c5ed3eeb2ca4a99824d1c5e01bd1c2325d6
chanhlee <chanhlee@student.42seoul.kr>
2021-01-27 15:49:44 +0900

    add ft_strrchr

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -6,8 +6,22 @@
 /*   By: chanhlee <chanhlee@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/26 19:46:11 by chanhlee          #+#    #+#             */
-/*   Updated: 2021/01/27 15:16:13 by chanhlee         ###   ########.fr       */
+/*   Updated: 2021/01/27 15:47:47 by chanhlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char	*ft_strrchr(const char *s, int c)
+{
+	int i;
+
+	i = ft_strlen(s);
+	while (i >= 0)
+	{
+		if (s[i] == (char)c)
+			return ((char*)(s + i));
+		i--;
+	}
+	return (NULL);
+}
