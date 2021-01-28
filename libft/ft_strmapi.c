fc25ebff332b8463f1a5a83934e8db27ccf64fa3
chanhlee <chanhlee@student.42seoul.kr>
2021-01-28 11:27:44 +0900

    add strmapi

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -6,10 +6,27 @@
 /*   By: chanhlee <chanhlee@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/26 11:53:28 by chanhlee          #+#    #+#             */
-/*   Updated: 2021/01/27 23:31:22 by chanhlee         ###   ########.fr       */
+/*   Updated: 2021/01/28 11:26:58 by chanhlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char *str;
+	int len;
+	int i;
 
+	len = ft_strlen(s);
+	if (!(str = (char*)malloc(sizeof(char)*(len + 1))))
+		return (NULL);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
