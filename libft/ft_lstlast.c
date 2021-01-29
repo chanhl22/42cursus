7efa56a10abbcb0ba2354829d1946f3d2645aded
chanhlee <chanhlee@student.42seoul.kr>
2021-01-29 20:56:02 +0900

    lstlast

--- libft/ft_lstlast.c
+++ libft/ft_lstlast.c
@@ -6,8 +6,19 @@
 /*   By: chanhlee <chanhlee@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/29 15:53:18 by chanhlee          #+#    #+#             */
-/*   Updated: 2021/01/29 15:53:19 by chanhlee         ###   ########.fr       */
+/*   Updated: 2021/01/29 20:55:50 by chanhlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+t_list *ft_lstlast(t_list *lst)
+{
+	while (lst != NULL)
+	{
+		lst = lst->next;
+		if (lst == NULL)
+			return (lst);
+	}
+	return (lst);
+}
