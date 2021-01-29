784d0e39be2dad2dd245cd0ae228378ef682f308
chanhlee <chanhlee@student.42seoul.kr>
2021-01-29 22:20:38 +0900

    update istadd_back

--- libft/ft_lstadd_back.c
+++ libft/ft_lstadd_back.c
@@ -6,8 +6,24 @@
 /*   By: chanhlee <chanhlee@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/29 15:53:35 by chanhlee          #+#    #+#             */
-/*   Updated: 2021/01/29 15:53:35 by chanhlee         ###   ########.fr       */
+/*   Updated: 2021/01/29 22:20:10 by chanhlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list *last;
+
+	if (lst != NULL)
+	{
+		if (*lst != NULL)
+		{
+			last = ft_lstlast(*lst)
+			laist->next = new;
+		}
+		else
+			*lst = new;
+	}
+}
