17c1ded22577af2a4a46faa8800cfe137c8915c5
chanhlee <chanhlee@student.42seoul.kr>
2021-01-30 00:09:29 +0900

    update lstdelone

--- libft/ft_lstdelone.c
+++ libft/ft_lstdelone.c
@@ -6,8 +6,15 @@
 /*   By: chanhlee <chanhlee@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/29 15:53:49 by chanhlee          #+#    #+#             */
-/*   Updated: 2021/01/29 15:53:50 by chanhlee         ###   ########.fr       */
+/*   Updated: 2021/01/30 00:09:11 by chanhlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	del(lst->content);
+	free(lst);
+	lst = NULL;
+}
