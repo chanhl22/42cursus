de40b1c4b3a316ecb51937decb9de20edd2d35ab
chanhlee <chanhlee@student.42seoul.kr>
2021-02-24 17:12:29 +0900

    first

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,8 +6,45 @@
 /*   By: chanhlee <chanhlee@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/24 09:42:53 by chanhlee          #+#    #+#             */
-/*   Updated: 2021/02/24 09:48:10 by chanhlee         ###   ########.fr       */
+/*   Updated: 2021/02/24 17:12:17 by chanhlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-test
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#define BUFSIZE 512
+
+/*
+int main()
+{
+   char buffer[BUFSIZE];
+   int fd;
+   ssize_t nread;
+   long total = 0;
+   char *backup:q
+
+   if ((fd = open("test.txt", O_RDONLY)) == -1)
+	   exit(1);
+   while ((nread = read(fd, buffer, BUFSIZE)) > 0)
+	   total += nread;
+   close(fd);
+   printf ("Number of characters in testfile : %ld\n", total);
+   exit(0);
+}
+*/
+
+int get_next_line(int fd, char **line)
+{
+	int nread;
+	char buffer[BUFSIZE];
+
+	if (fd < 0)
+		return (-1);
+	while ((nread = read(fd, buf, BUFSIZE)) > 0)
+	{
+		if (
+	}
+
+}
