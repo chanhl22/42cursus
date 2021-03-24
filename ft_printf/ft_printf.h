615eff87b603e6bdf435f20627066d6a11bb6764
Chanhee Lee <chanhlee@student.42seoul.kr>
2021-03-24 17:01:25 +0900

    start printf

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -1,24 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: chanhlee <chanhlee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/24 14:27:57 by chanhlee          #+#    #+#             */
-/*   Updated: 2021/03/24 16:55:49 by chanhlee         ###   ########.fr       */
+/*   Created: 2021/03/24 17:00:36 by chanhlee          #+#    #+#             */
+/*   Updated: 2021/03/24 17:00:38 by chanhlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdarg.h>
-
-int	ft_printf(const char *restrict format, ...)
-{
-	va_list ap;
-	int result;
-
-	va_start(ap, format);
-	result = ft_print(format, ap);
-	va_end(ap);
-	return (result);
-}
