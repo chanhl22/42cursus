7472112528ae7850344f798f0aca009daa720014
chanhl22 <chanhlee@student.42seoul.kr>
2021-05-24 09:22:10 +0900

    test error

--- ft_server/srcs/run.sh
+++ ft_server/srcs/run.sh
@@ -15,27 +15,27 @@ chmod 600 etc/ssl/certs/localhost.dev.crt etc/ssl/private/localhost.dev.key
 cp -rp /tmp/default /etc/nginx/sites-available/
 
 # wordpress 설치 및 설정
-wget https://wordpress.org/latest.tar.gz
-tar -xvf latest.tar.gz
-mv wordpress/ var/www/html/
-chown -R www-data:www-data /var/www/html/wordpress
-cp -rp ./tmp/wp-config.php /var/www/html/wordpress
+#wget https://wordpress.org/latest.tar.gz
+#tar -xvf latest.tar.gz
+#mv wordpress/ var/www/html/
+#chown -R www-data:www-data /var/www/html/wordpress
+#cp -rp ./tmp/wp-config.php /var/www/html/wordpress
 
 # wordpress를 위한 DB 테이블 생성 
-service mysql start
-echo "CREATE DATABASE IF NOT EXISTS wordpress;" \
-	| mysql -u root --skip-password
+#service mysql start
+#echo "CREATE DATABASE IF NOT EXISTS wordpress;" \
+#	| mysql -u root --skip-password
 	
 # phpMyAdmin 설치 및 설정
-wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.tar.gz
-tar -xvf phpMyAdmin-5.0.2-all-languages.tar.gz
-mv phpMyAdmin-5.0.2-all-languages phpmyadmin
-mv phpmyadmin /var/www/html/
-rm phpMyAdmin-5.0.2-all-languages.tar.gz
-cp -rp /tmp/config.inc.php /var/www/html/phpmyadmin/
+#wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.tar.gz
+#tar -xvf phpMyAdmin-5.0.2-all-languages.tar.gz
+#mv phpMyAdmin-5.0.2-all-languages phpmyadmin
+#mv phpmyadmin /var/www/html/
+#rm phpMyAdmin-5.0.2-all-languages.tar.gz
+#cp -rp /tmp/config.inc.php /var/www/html/phpmyadmin/
 
 service nginx start
 service php7.3-fpm start
-service mysql restart
+#service mysql restart
 
 bash
